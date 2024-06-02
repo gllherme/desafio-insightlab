8c5f3da8d7d3003f6d0eea204854fd13d647f29e
gllherme <guicampos159@gmail.com>
2024-06-01 23:34:34 -0300

    include routers and exceptions

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,31 +1,16 @@
 from fastapi import FastAPI
-from app.config import COUNTRY_PROFILE_BASE_URL
 
-from app.services.country_codes import get_all_country_codes
-from app.services.country_profile import get_country_profile
-from app.services.indicators import get_all_indicators
-from app.models.country_code import CountryCode
-from app.models.country_profile import CountryProfile
-from app.models.indicator import IndicatorGroup
+from app.exceptions.no_data_exception import NoDataException, no_data_exception_handler
+from app.routers import country, indicator
 
 app = FastAPI()
+
+app.add_exception_handler(NoDataException, no_data_exception_handler)
+
+app.include_router(country.router)
+app.include_router(indicator.router)
 
 
 @app.get("/")
 async def status():
-    return {"Ok"}
-
-
-@app.get("/country/profile/{code}", tags=["Country"])
-async def get_country_profile_by_country_code(code: str) -> CountryProfile:
-    return get_country_profile(code)
-
-
-@app.get("/country/codes", tags=["Utils"])
-async def get_country_codes() -> list[CountryCode]:
-    return get_all_country_codes()
-
-
-@app.get("/indicators/all", tags=["Utils"])
-async def get_all_indicators_grouped() -> IndicatorGroup:
-    return get_all_indicators()
+    return {"status": "Ok"}
