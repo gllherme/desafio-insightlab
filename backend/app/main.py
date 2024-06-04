53b5e44112f697578dd10604b0f7f1c448f7afec
gllherme <guicampos159@gmail.com>
2024-06-03 21:27:36 -0300

    exception nos parametros do filtro de data

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 
-from app.exceptions.no_data_exception import NoDataException, no_data_exception_handler
 from app.routers import country, indicator
+
+from app.exceptions.no_data_exception import NoDataException, no_data_exception_handler
+from app.exceptions.bad_filter_value_exception import BadFilterValueException, bad_filter_exception_handler
 
 app = FastAPI()
 
 app.add_exception_handler(NoDataException, no_data_exception_handler)
+app.add_exception_handler(BadFilterValueException,
+                          bad_filter_exception_handler)
 
 app.include_router(country.router)
 app.include_router(indicator.router)
