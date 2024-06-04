e90e814ed9ecfdd240eb5469e9f0ba87423d9193
gllherme <guicampos159@gmail.com>
2024-06-03 21:28:53 -0300

    testes

---
+++ backend/app/tests/test_country_service.py
@@ -0,0 +1,25 @@
+from app.tests.test_main import client
+
+from app.models.country_code import CountryCode
+from app.models.country_data import CountryData
+
+
+def test_read_country_codes():
+    response = client.get("/country/codes")
+    assert response.status_code == 200
+    countries = response.json()
+
+    assert isinstance(countries, list)
+
+    for c in countries:
+        assert isinstance(c, dict)
+
+        assert CountryCode(**c)
+
+
+def test_read_country_data():
+    response = client.get("/country/BR/query/77849")
+    assert response.status_code == 200
+    country_data = response.json()
+
+    assert isinstance(country_data, dict)
