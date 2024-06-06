7d03e46c9c51e8e66e53874d3d058dcd901f9368
gllherme <guicampos159@gmail.com>
2024-06-06 18:29:57 -0300

    header component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.text}>Desafio Insight Lab</div>
+    </header>
+  );
+}
