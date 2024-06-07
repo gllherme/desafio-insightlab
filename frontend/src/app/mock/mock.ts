52abedfc0b2b50c0381e52d45844b1bbbe07fded
gllherme <guicampos159@gmail.com>
2024-06-07 17:50:55 -0300

    pagina perfil de país com dados mockados

---
+++ frontend/src/app/mock/mock.ts
@@ -0,0 +1,9 @@
+export const mock = {
+  code: "BR",
+  name: "Brasil",
+  area: 8510345.538,
+  region: "América",
+  languages: ["português"],
+  currencies: ["Real brasileiro"],
+  text: "",
+};
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,72 +1,34 @@
-import { Column } from "react-table";
-import styles from "./page.module.css";
-import Table from "@/components/Table";
-import { useMemo } from "react";
 import Autocomplete from "@/components/Autocomplete";
-import Select from "@/components/GroupSelect";
+import TagSlot from "@/components/TagSlot";
+import { mock } from "./mock/mock";
+import styles from "./page.module.css";
 
 export default function Home() {
-  const data = useMemo(
-    () => [
-      {
-        col1: "Hello",
-        col2: "World",
-      },
-
-      {
-        col1: "react-table",
-        col2: "rocks",
-      },
-
-      {
-        col1: "whatever",
-        col2: "you want",
-      },
-    ],
-    []
-  );
-
-  const columns: Array<Column> = useMemo(
-    () => [
-      { Header: "Column 1", accessor: "col1" },
-      { Header: "Column 2", accessor: "col2" },
-    ],
-    []
-  );
-
-  const options = {
-    Economia: [
-      {
-        id: 10120,
-        name: "nome",
-      },
-    ],
-    População: [
-      {
-        id: 1934,
-        name: "pop",
-      },
-    ],
-  };
-
-  const sla = [
-    {
-      category: "category name",
-      values: [
-        { id: 112323, name: "kadkmafa" },
-        { id: 112323, name: "asdfhasbd" },
-      ],
-    },
-  ];
-
   return (
     <main className={styles.main}>
-      <div>
-        <div className={styles.inputs}>
-          <Autocomplete suggestions={["a", "aaaaa", "b", "c"]} />
-          <Select options={sla} />
+      <div className={styles.wrapper}>
+        <Autocomplete suggestions={[]} />
+        <div className={styles.card}>
+          <section>
+            <h3>
+              {mock.name} ({mock.code}) - {mock.region}
+            </h3>
+            <div>Area Territorial: {mock.area.toLocaleString()} km²</div>
+          </section>
+          <section>
+            <h4>Linguas</h4>
+            <TagSlot tags={mock.languages} />
+          </section>
+          <section>
+            <h4>Moedas</h4>
+            <TagSlot tags={mock.currencies} />
+          </section>
+
+          <h4>Historico:</h4>
+          <textarea className={styles.textarea} readOnly>
+            {mock.text}
+          </textarea>
         </div>
-        <Table data={data} columns={columns} />
       </div>
     </main>
   );
