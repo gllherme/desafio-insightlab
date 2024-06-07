7dbb185f648192ff5026469f5d3a5f11dea99d40
gllherme <guicampos159@gmail.com>
2024-06-07 16:37:39 -0300

    navigation

---
+++ frontend/src/components/Header/Navigation/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./navigation.module.css";
+import { usePathname } from "next/navigation";
+
+export interface NavLink {
+  name: string;
+  href: string;
+}
+
+interface NavigationProps {
+  navLinks: Array<NavLink>;
+}
+
+interface NavItemProps {
+  navLink: NavLink;
+}
+
+export default function Navigation({ navLinks }: NavigationProps) {
+  return (
+    <>
+      {navLinks.map((link) => (
+        <NavItem navLink={link} />
+      ))}
+    </>
+  );
+}
+
+function NavItem({ navLink }: NavItemProps) {
+  const pathname = usePathname();
+  const isActive = pathname === navLink.href;
+
+  return (
+    <Link
+      className={isActive ? styles.active : styles.default}
+      href={navLink.href}
+    >
+      <p>{navLink.name}</p>
+    </Link>
+  );
+}
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,9 +1,22 @@
+import Navigation from "./Navigation";
 import styles from "./header.module.css";
+
+const navLinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Indicadores",
+    href: "/country",
+  },
+];
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.text}>Desafio Insight Lab</div>
+      <Navigation navLinks={navLinks} />
     </header>
   );
 }
