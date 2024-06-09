904d5d4e24fc7d9d28b22f35fd512d03366aeac9
gllherme <guicampos159@gmail.com>
2024-06-08 21:09:15 -0300

    integração pagina principal e login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,79 @@
 "use client";
 
-import Autocomplete from "@/components/Autocomplete";
+import api from "@/api/api";
+import Select from "@/components/Select";
 import TagSlot from "@/components/TagSlot";
 import isAuth from "@/components/isAuth";
-import { mock } from "./mock/mock";
+import { ChangeEvent, useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 function Home() {
+  const token = localStorage.getItem("accessToken");
+  const [options, setOptions] = useState([]);
+  const [countryProfile, setCountryProfile] = useState<any>({});
+
+  const fetchSuggestions = async () => {
+    try {
+      const { data } = await api.get("/country/codes", {
+        headers: { Authorization: "Bearer " + token },
+      });
+      const newOptions = data.map((option: any) => ({
+        id: option.code,
+        name: option.name,
+      }));
+      setOptions(newOptions);
+    } catch (e) {
+      setOptions([]);
+    }
+  };
+
+  const fetchProfile = async (e: ChangeEvent<HTMLSelectElement>) => {
+    const id = e.target.options[e.target.selectedIndex].id;
+
+    try {
+      const { data } = await api.get("/country/profile/" + id, {
+        headers: { Authorization: "Bearer " + token },
+      });
+      setCountryProfile(data);
+    } catch (e) {
+      setCountryProfile({});
+    }
+  };
+
+  useEffect(() => {
+    fetchSuggestions();
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.wrapper}>
-        <Autocomplete suggestions={[]} />
+        <Select options={options} onChange={fetchProfile} />
         <div className={styles.card}>
           <section>
             <h3>
-              {mock.name} ({mock.code}) - {mock.region}
+              {countryProfile.name || ""} ({countryProfile.code || ""}) -{" "}
+              {countryProfile.region || ""}
             </h3>
-            <div>Area Territorial: {mock.area.toLocaleString()} km²</div>
+            <div>
+              Area Territorial: {countryProfile.area?.toLocaleString() || ""}{" "}
+              km²
+            </div>
           </section>
           <section>
             <h4>Linguas</h4>
-            <TagSlot tags={mock.languages} />
+            <TagSlot tags={countryProfile.languages || []} />
           </section>
           <section>
             <h4>Moedas</h4>
-            <TagSlot tags={mock.currencies} />
+            <TagSlot tags={countryProfile.currencies || []} />
           </section>
 
           <h4>Historico:</h4>
-          <textarea className={styles.textarea} readOnly>
-            {mock.text}
-          </textarea>
+          <textarea
+            className={styles.textarea}
+            value={countryProfile.text || ""}
+            readOnly
+          />
         </div>
       </div>
     </main>
--- frontend/src/components/LoginForm/index.tsx
+++ frontend/src/components/LoginForm/index.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import api from "@/api/api";
+import { useRouter } from "next/navigation";
 import { ChangeEvent, useState } from "react";
 import Button from "../Button";
 import Input from "../Input";
 import styles from "./loginform.module.css";
 
 export default function LoginForm() {
+  const router = useRouter();
   const [values, setValues] = useState({ username: "", password: "" });
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -18,19 +21,32 @@ export default function LoginForm() {
     }));
   };
 
-  const submit = () => {
-    console.log(values);
+  const submit = async () => {
+    try {
+      const { data } = await api.post("/auth/token", values);
+      localStorage.setItem("accessToken", data.access_token);
+      router.push("/");
+    } catch (e: any) {
+      localStorage.removeItem("accessToken");
+      setValues({ username: "", password: "" });
+      alert(e.response.data.detail);
+    }
   };
 
   return (
     <div className={styles.card}>
       <section>
         <h4>Username</h4>
-        <Input id="username" onChange={handleChange} />
+        <Input id="username" value={values.username} onChange={handleChange} />
       </section>
       <section>
         <h4>Senha</h4>
-        <Input id="password" type="password" onChange={handleChange} />
+        <Input
+          id="password"
+          value={values.password}
+          type="password"
+          onChange={handleChange}
+        />
       </section>
       <Button text="Login" type="button" onClick={submit} />
     </div>
