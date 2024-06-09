44a2dfe53ef35a3e84c489ae77a348452d3299e3
gllherme <guicampos159@gmail.com>
2024-06-08 21:08:19 -0300

    componente de autenticação das paginas

--- frontend/src/components/isAuth/index.tsx
+++ frontend/src/components/isAuth/index.tsx
@@ -1,20 +1,38 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import api from "@/api/api";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-
-const isAuthenticated = false;
 
 export default function isAuth(Component: React.FC) {
   return function isAuth(props: any) {
-    const auth = isAuthenticated;
+    const router = useRouter();
+    const token = localStorage.getItem("accessToken");
+    const isAuth = token !== "undefined" && token !== null;
+
+    const checkAuth = async () => {
+      try {
+        const { status } = await api.get("/auth/me", {
+          headers: { Authorization: "Bearer " + token },
+        });
+        if (status === 200) {
+          return;
+        }
+      } catch (e) {
+        localStorage.removeItem("accessToken");
+        router.push("/auth");
+      }
+    };
 
     useEffect(() => {
-      if (!auth) {
-        return redirect("/auth");
+      checkAuth();
+
+      if (!isAuth) {
+        return router.push("/auth");
       }
     }, []);
-    if (!auth) {
+
+    if (!isAuth) {
       return null;
     }
 
